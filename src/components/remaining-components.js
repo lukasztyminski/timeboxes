32fb5e80116213f05e46d13ffb9f9631d3ae3a11
Kurs Reacta <kursreacta@MacBook-Pro-3.local>
2019-05-05 14:40:21 +0200

    Extract TimeboxCreator

--- src/components/remaining-components.js
+++ src/components/remaining-components.js
@@ -1,48 +1,8 @@
 import React from "react";
-import uuid from "uuid";
 
 import Timebox from "./Timebox";
+import TimeboxCreator from "./TimeboxCreator";
 
-class TimeboxCreator extends React.Component {
-    constructor(props) {
-        super(props);
-        this.titleInput = React.createRef();
-        this.totalTimeInMinutesInput = React.createRef();
-    }
-    handleSubmit = (event) => {
-        event.preventDefault(); 
-        this.props.onCreate({ 
-            id: uuid.v4(), 
-            title: this.titleInput.current.value, 
-            totalTimeInMinutes: this.totalTimeInMinutesInput.current.value
-        });
-        this.titleInput.current.value = "";
-        this.totalTimeInMinutesInput.current.value = "";
-    }
-
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit} className="TimeboxCreator">
-                <label>
-                    Co robisz?
-                    <input 
-                        ref={this.titleInput}
-                        type="text" 
-                    />
-                </label><br/>
-                <label>
-                    Ile minut?
-                    <input 
-                        ref={this.totalTimeInMinutesInput}
-                        type="number" 
-                    />
-                </label><br />
-                <button 
-                >Dodaj timebox</button>
-            </form>
-        )
-    }
-}
 class TimeboxList extends React.Component {
     state = {
         timeboxes: [
