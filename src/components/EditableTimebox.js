a197c9d22bbdd0f2344569070b7576f01719b350
Kurs Reacta <kursreacta@MacBook-Pro-3.local>
2019-05-05 12:16:39 +0200

    Extract TimeboxEditor

--- src/components/EditableTimebox.js
+++ src/components/EditableTimebox.js
@@ -1,43 +1,7 @@
 import React from "react";
 import Clock from "./Clock";
 import ProgressBar from "./ProgressBar";
-
-function TimeboxEditor(props) {
-    const { 
-        title, 
-        totalTimeInMinutes,
-        isEditable,
-        onTitleChange,
-        onTotalTimeInMinutesChange,
-        onConfirm
-    } = props;
-    return (
-        <div className={`TimeboxEditor ${isEditable ? "" : "inactive"}`}>
-            <label>
-                Co robisz?
-                <input 
-                    disabled={!isEditable} 
-                    value={title}
-                    onChange={onTitleChange} 
-                    type="text" 
-                />
-            </label><br/>
-            <label>
-                Ile minut?
-                <input 
-                    disabled={!isEditable} 
-                    value={totalTimeInMinutes}
-                    onChange={onTotalTimeInMinutesChange} 
-                    type="number" 
-                />
-            </label><br />
-            <button 
-                onClick={onConfirm}
-                disabled={!isEditable}
-            >Zatwierdź zmiany</button>
-        </div>
-    )
-}
+import TimeboxEditor from "./TimeboxEditor";
 
 class CurrentTimebox extends React.Component {
     constructor(props) {
