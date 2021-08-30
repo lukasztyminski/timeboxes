8c171a0394ad4a9772db8011fbd95036b517c313
ltyminski <lttyminski@gmail.com>
2021-08-30 12:47:56 +0200

    Updates

---
+++ precache-manifest.7c7b71e05bdca85eaa21217fd96a24ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dabaa6db033210f87a56137fec237cd7",
+    "url": "/timeboxes/index.html"
+  },
+  {
+    "revision": "f416becbf47f4a7be0c4",
+    "url": "/timeboxes/static/css/main.0cc6e265.chunk.css"
+  },
+  {
+    "revision": "0973c5c907c84397b512",
+    "url": "/timeboxes/static/js/2.dd531b93.chunk.js"
+  },
+  {
+    "revision": "3fd7e288cb6df83dd55c",
+    "url": "/timeboxes/static/js/3.da075412.chunk.js"
+  },
+  {
+    "revision": "f416becbf47f4a7be0c4",
+    "url": "/timeboxes/static/js/main.73475172.chunk.js"
+  },
+  {
+    "revision": "fc88d90270ac60b218ea",
+    "url": "/timeboxes/static/js/runtime~main.1e115d3e.js"
+  }
+]);
